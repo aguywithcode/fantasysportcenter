4a43624889e7b66ad9a310f5eeb2b1d454bc58ed
Michael Brown <micbrown@microsoft.com>
2022-05-13 14:56:23 -0700

    added simple list of teams with logos

--- web/src/App.js
+++ web/src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var teams = [
+    {
+      logoUrl: "https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/bulls/reboot/resources/images/bullhead-1819.png",
+      city: "Chicago",
+      mascot: "Bulls",
+    },
+    {
+      logoUrl: "https://www.nba.com/grizzlies/sites/grizzlies/files/mg_playoffs_100x100_icon_0.png?",
+      city: "Memphis",
+      mascot: "Grizzlies",
+    },
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
-          Edit <code>src/App.js</code> and save to reload.
+          <ul>
+            {
+              teams.map(t => {
+                return(
+                  <li>
+                    <img src={t.logoUrl} alt={t.mascot+" Logo"} /> {t.city} {t.mascot}
+                  </li>
+                );
+              })
+            }
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

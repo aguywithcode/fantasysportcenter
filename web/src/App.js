9997ec6853e3b25418c49b8701d52f662d5eae1a
Michael Brown <mbrown77@gmail.com>
2022-06-25 15:03:06 -0700

    Moved team list into separate component

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import {faker} from '@faker-js/faker';
+import TeamList from './TeamList.js';
 
 function App() {
-  var teams = [...Array(10)].map(i => {
+  var teams = [...Array(5)].map(i => {
     var mascot = faker.animal.type();
     return {
       logoUrl: faker.image.imageUrl(300, 300,mascot, true),
@@ -15,18 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          <ul>
-            {
-              teams.map(t => {
-                return(
-                  <li>
-                    <div><img src={t.logoUrl} alt={t.mascot+" Logo"} /></div> 
-                    <div>{t.city} {t.mascot}</div>
-                  </li>
-                );
-              })
-            }
-          </ul>
+          <TeamList teams={teams}/>
         </p>
       </header>
     </div>

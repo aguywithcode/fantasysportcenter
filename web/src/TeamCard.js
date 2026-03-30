0adb8068e2da05b7b38da6462973fa14cc8240fa
Michael Brown <aguywhocodes@outlook.com>
2026-03-30 06:39:21 -0700

    I don't know why I'm committing this. It's going to be written over from the ground up

--- web/src/TeamCard.js
+++ web/src/TeamCard.js
@@ -3,12 +3,13 @@ import Card from 'react-bootstrap/Card';
 function TeamCard(props){
     const t = props.team;
     return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Img src={t.LogoUrl} alt={t.Name + " Logo"}/>
-            <Card.Body>
-                <Card.Title>{t.City} {t.Name}</Card.Title>
-                {t.Division}
-            </Card.Body>
+        <Card style={{ width: '14rem', margin: '0.5rem', fontSize: '0.95em', display: 'flex', alignItems: 'center', minHeight: '110px' }}>
+            <div style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                <img src={t.LogoUrl} alt={t.Name + " Logo"} style={{ width: '100px', height: '100px', objectFit: 'contain', marginRight: '0.75rem', background: '#fff', borderRadius: '8px', border: '1px solid #eee' }} />
+                <div>
+                    <div style={{ fontWeight: 'bold', fontSize: '1.1em' }}>{t.City} {t.Name}</div>
+                </div>
+            </div>
         </Card>
     )
 }
--- web/src/TeamList.js
+++ web/src/TeamList.js
@@ -1,25 +1,41 @@
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col'
 import './App.css';
 import TeamCard from './TeamCard';
 
 function TeamList(props) {
     const teams = props.teams;
+    // Group teams by conference, then division
+    const conferences = {};
+    teams.forEach(team => {
+        if (!conferences[team.Conference]) {
+            conferences[team.Conference] = {};
+        }
+        if (!conferences[team.Conference][team.Division]) {
+            conferences[team.Conference][team.Division] = [];
+        }
+        conferences[team.Conference][team.Division].push(team);
+    });
+
     return (
         <Container>
             <Row>
-                {
-                    teams.sort((a,b)=>{
-                        if(a.Division>b.Division){
-                            return 1;
-                        }
-                        if(a.Division<b.Division){
-                            return -1;
-                        }
-                        return 0;
-                    })
-                        .map((element) => <TeamCard team={element} />)
-                }
+                {Object.keys(conferences).sort().map(conference => (
+                    <Col key={conference} md={6} style={{ marginBottom: '2rem' }}>
+                        <h3 style={{ margin: '1rem 0' }}>{conference}</h3>
+                        {Object.keys(conferences[conference]).sort().map(division => (
+                            <div key={division} style={{ marginBottom: '1.5rem' }}>
+                                <h4 style={{ margin: '0.5rem 0' }}>{division}</h4>
+                                <Row>
+                                    {conferences[conference][division].map(team => (
+                                        <TeamCard key={team._id || team.Name} team={team} />
+                                    ))}
+                                </Row>
+                            </div>
+                        ))}
+                    </Col>
+                ))}
             </Row>
         </Container>
     );
